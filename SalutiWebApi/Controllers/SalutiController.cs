c5eab4d805dd7d3f5d66a651b2dd947143c43950
Test-SpringCloud <nicola_larocca@xantrix.it>
2021-06-21 17:50:50 +0200

    Aggiunte info log

--- SalutiWebApi/Controllers/SalutiController.cs
+++ SalutiWebApi/Controllers/SalutiController.cs
@@ -21,6 +21,7 @@ namespace SalutiWebApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<InfoMsg> getSaluti2(string Nome)  
         {
+            Console.WriteLine("Otteniamo il nome dell'utente");
             return Ok(new InfoMsg(DateTime.Today, $"Saluti {Nome}, sono la tua web api aggiornata con GitHub in Azure App Services"));
         }
 
@@ -28,6 +29,7 @@ namespace SalutiWebApi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<InfoMsg> getSaluti3(string Nome, string version)
         {
+            Console.WriteLine("Otteniamo il nome dell'utente e la versione della web api");
             return Ok(new InfoMsg(DateTime.Today, $"Saluti {Nome}, sono la versione {version} della web api aggiornata con con GitHub"));
         }
     }
